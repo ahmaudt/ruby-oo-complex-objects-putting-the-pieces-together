850f30dd2c2a33a6cc7a72c8fece46204e57c2c2
Ahmaud R. Templeton <ahmaud@gmail.com>
2021-01-29 20:44:12 -0500

    commit tree a5890a1b31b167213d6c285540b8ef25bafe6473

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,1 +1,19 @@
 # Make your shoe class here!
+class Shoe
+
+    attr_accessor :brand, :color, :size, :material, :condition
+
+    def initialize(brand)
+        @brand = brand
+    end
+
+    def cobble
+        if @condition="old"
+            puts "Your shoe is as good as new!"
+            return @condition = "new"
+        end
+    end
+
+
+
+end
